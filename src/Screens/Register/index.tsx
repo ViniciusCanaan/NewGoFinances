e4fbb319ed0cd2dd6aaac026ef3373369aa6027f
Vinicius Canaan <60820733+ViniciusCanaan@users.noreply.github.com>
2022-08-12 00:28:37 -0300

    Inserindo Botoes

--- src/Screens/Register/index.tsx
+++ src/Screens/Register/index.tsx
@@ -1,27 +1,54 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Button } from "../../components/Form/Button";
 import { Input } from "../../components/Form/Input";
+import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
 
 import {
     Container,
     Header,
     Title,
-    Form
+    Form,
+    Fields,
+    TransactionsTypes,
 } from "./styles";
 
 export function Register() {
+    const [transactionType, setTransactionType] = useState('');
+
+    function handleTransactionTypeSelect(type: 'up' | 'down') {
+        setTransactionType(type);
+    }
+
     return (
         <Container>
             <Header>
                 <Title>Cadastro</Title>
             </Header>
             <Form>
-                <Input
-                    placeholder="Nome"
-                />
-                <Input
-                    placeholder="Preço"
-                />
+                <Fields>
+                    <Input
+                        placeholder="Nome"
+                    />
+                    <Input
+                        placeholder="Preço"
+                    />
+
+                    <TransactionsTypes>
+                        <TransactionTypeButton
+                            type='up'
+                            title="Income"
+                            onPress={() => handleTransactionTypeSelect('up')}
+                            isActive={transactionType === 'up'}
+                        />
+                        <TransactionTypeButton
+                            type='down'
+                            title="Outcome"
+                            onPress={() => handleTransactionTypeSelect('down')}
+                            isActive={transactionType === 'down'}
+                        />
+                    </TransactionsTypes>
+                </Fields>
+                <Button title="Enviar" />
             </Form>
         </Container>
     );
